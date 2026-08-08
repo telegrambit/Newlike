cd7605060538f2dcb786819d3aa71d5059df64ae
Jeherul Isalm <jeherulislam0069@gmail.com>
2026-08-08 12:33:00 +0530

    Update valid API key in app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 
 # ✅ Valid API keys
 VALID_API_KEYS = {
-    "saito"  # don't change warna api or bot dono nhi chalega 
+    "islam"  # don't change warna api or bot dono nhi chalega 
 }
 
 # 🔢 Like limit tracking
